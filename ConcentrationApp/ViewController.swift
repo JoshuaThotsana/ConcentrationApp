3e4bd23404050fd612d7b9d296bd912cd20f2fda
Thotsana <thosana.mabotsa@umuzi.org>
2020-11-09 21:20:56 +0200

    End of First Lecture

--- ConcentrationApp/ViewController.swift
+++ ConcentrationApp/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+	
+	var flipCount = 0 {
+		didSet {
+			flipCountLabel.text = "Flips: \(flipCount)"
+		}
 	}
-
-
+	
+	@IBOutlet weak var flipCountLabel: UILabel!
+	
+	@IBOutlet var cardButtons: [UIButton]!
+	
+	var emojiChoices = ["👻","🎃","👻","🎃"]
+	
+	@IBAction func touchCard(_ sender: UIButton) {
+		flipCount += 1
+		if let cardNumber = cardButtons.index(of: sender) {
+			flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+		}
+	}
+	
+	func flipCard(withEmoji emoji: String, on button: UIButton) {
+		if button.currentTitle == emoji {
+			button.setTitle("", for: UIControl.State.normal)
+			button.backgroundColor = UIColor.orange
+		} else {
+			button.setTitle(emoji, for: UIControl.State.normal)
+			button.backgroundColor = UIColor.white
+		}
+	}
+	
 }
 
